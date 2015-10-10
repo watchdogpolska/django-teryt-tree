b139759e3aa8107177c45e6b1b53cef14bf042fc
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-10-10 17:54:13 +0200

    Fix import in factory/factories

--- tests/test_factories.py
+++ tests/test_factories.py
@@ -1,11 +1,11 @@
-from teryt_tree import factory
+from teryt_tree import factories
 from django.test import TestCase
 
 
 class JednostkaAdministracyjnaFactoryTestCase(TestCase):
     def test_parent_settings(self):
-        obj1 = factory.JednostkaAdministracyjnaFactory()
-        obj2 = factory.JednostkaAdministracyjnaFactory(parent=obj1)
+        obj1 = factories.JednostkaAdministracyjnaFactory()
+        obj2 = factories.JednostkaAdministracyjnaFactory(parent=obj1)
         self.assertEqual(obj1.level, 0)
         self.assertEqual(obj2.level, 1)
         self.assertEqual(obj2.parent, obj1)
