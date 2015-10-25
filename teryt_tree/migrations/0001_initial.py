480e313dfec6441c68a060dac78012192ab19529
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-10-26 00:32:14 +0100

    Squeeze migrations

--- teryt_tree/migrations/0001_initial.py
+++ teryt_tree/migrations/0001_initial.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.db import migrations, models
 import autoslug.fields
 import mptt.fields
-from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
@@ -15,32 +15,34 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Category',
             fields=[
-                ('id', models.AutoField(verbose_name='ID',
-                                        serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=50)),
                 ('slug', autoslug.fields.AutoSlugField(populate_from=b'name', editable=False)),
-                ('level', models.IntegerField()),
+                ('level', models.IntegerField(db_index=True, choices=[(1, 'wojew\xf3dztwo'), (2, 'powiat'), (3, 'gmina')])),
             ],
+            options={
+                'verbose_name': 'Kategoria',
+                'verbose_name_plural': 'Kategorie',
+            },
         ),
         migrations.CreateModel(
             name='JednostkaAdministracyjna',
             fields=[
                 ('id', models.CharField(max_length=7, serialize=False, primary_key=True)),
-                ('name', models.CharField(max_length=36, verbose_name=b'Nazwa')),
-                ('slug', autoslug.fields.AutoSlugField(populate_from=b'name', editable=False)),
-                ('updated_on', models.DateField()),
+                ('name', models.CharField(max_length=36, verbose_name='Nazwa')),
+                ('slug', autoslug.fields.AutoSlugField(populate_from=b'name', unique=True, editable=False)),
+                ('updated_on', models.DateField(verbose_name='Data aktualizacji')),
                 ('active', models.BooleanField(default=False)),
                 ('lft', models.PositiveIntegerField(editable=False, db_index=True)),
                 ('rght', models.PositiveIntegerField(editable=False, db_index=True)),
                 ('tree_id', models.PositiveIntegerField(editable=False, db_index=True)),
                 ('level', models.PositiveIntegerField(editable=False, db_index=True)),
                 ('category', models.ForeignKey(to='teryt_tree.Category')),
-                ('parent', mptt.fields.TreeForeignKey(related_name='children',
-                                                      blank=True, to='teryt_tree.JednostkaAdministracyjna', null=True)),
+                ('parent', mptt.fields.TreeForeignKey(related_name='children', blank=True, to='teryt_tree.JednostkaAdministracyjna', null=True)),
             ],
             options={
-                'verbose_name': 'TERYT',
-                'verbose_name_plural': 'TERYT',
+                'verbose_name': 'Jednostka podzia\u0142u terytorialnego',
+                'verbose_name_plural': 'Jednostki podzia\u0142u terytorialnego',
             },
         ),
     ]
