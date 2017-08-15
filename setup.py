4d40734a0e3bb1aec41ae7e48c26591d639a7c90
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-08-15 19:01:41 +0200

    Bump version: 0.13.2 → 0.14.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = '0.13.2'
+version = '0.14.0'
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
--- teryt_tree/__init__.py
+++ teryt_tree/__init__.py
@@ -1,2 +1,2 @@
-__version__ = '0.13.2'
+__version__ = '0.14.0'
 default_app_config = 'teryt_tree.apps.TerytTreeConfig'
