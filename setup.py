3b1cfec636e91a7e049b4862bca00dce554ae53a
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-01-07 01:48:50 +0100

    Bump version: 0.15.1 → 0.16.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = '0.15.1'
+version = '0.16.0'
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
